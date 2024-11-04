6dc89c8a63a130621b9907f9fd00520bb5039a20
José Fernando Resendiz López <56377740+Gerbo67@users.noreply.github.com>
2024-11-04 16:52:36 -0600

    Phaser step 3

--- src/scenes/Bootloader.js
+++ src/scenes/Bootloader.js
@@ -5,7 +5,6 @@ class Bootloader extends Phaser.Scene {
         });
     }
 
-
     init() {
         console.log('Cargando Init');
         console.log('Scene Bootloader');
@@ -13,8 +12,8 @@ class Bootloader extends Phaser.Scene {
 
     preload() {
         console.log('Soy Preload');
-        this.load.image("Bird1", "./assets/bird.png") // Permite instanciar la figura al cargarla en el cache
-        this.load.image("background", "./assets/landscape.png")
+        this.load.image("Bird1", "./assets/bird.png"); // Permite instanciar la figura al cargarla en el cache
+        this.load.image("background", "./assets/landscape.png");
     }
 
     create() {
@@ -26,44 +25,64 @@ class Bootloader extends Phaser.Scene {
             .setDisplaySize(this.scale.width, this.scale.height);
 
         this.variabledetener = false;
-        this.bird = this.physics.add.image(200, 200, "Bird1"); //Incluye una imagen con fisicas dando un lugar en el espacio y un nombre
-        //this.physics.add.existing(this.bird, false);
-        this.bird.body.setCollideWorldBounds(true);
+
+        // Crear el primer pájaro controlado con WASD
+        this.bird = this.physics.add.image(200, 200, "Bird1");
+        this.bird.setCollideWorldBounds(true);
+        this.bird.setImmovable(false); // Hacer que el objeto pueda moverse pero colisione
+        this.bird.setBounce(0);  // Sin rebote al colisionar para efecto sólido
+
+        // Crear el segundo pájaro controlado con las flechas
+        this.bird2 = this.physics.add.image(400, 200, "Bird1");
+        this.bird2.setCollideWorldBounds(true);
+        this.bird2.setImmovable(false); // Hacer que el objeto pueda moverse pero colisione
+        this.bird2.setBounce(0);  // Sin rebote al colisionar para efecto sólido
 
 
+        this.physics.add.collider(this.bird, this.bird2);
+
+        // Teclas de movimiento para bird (WASD)
         const keyCodes = Phaser.Input.Keyboard.KeyCodes;
-
         this.TA = this.input.keyboard.addKey(keyCodes.A);
         this.TD = this.input.keyboard.addKey(keyCodes.D);
         this.TW = this.input.keyboard.addKey(keyCodes.W);
         this.TS = this.input.keyboard.addKey(keyCodes.S);
+
+        // Teclas de movimiento para bird2 (flechas)
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update(time, delta) {
-        if (!this.variabledetener) {
-            //console.log("Se esta actualizando el programa");
-            this.variabledetener = true;
-        }
+        // Movimiento de bird (controlado con WASD)
         if (this.TA.isDown) {
-            //console.log("Se presiono Tecla A");
-            this.bird.x -= 2;
+            this.bird.setVelocityX(-100);
             this.bird.flipX = true;
-        }
-        if (this.TD.isDown) {
-            //console.log("Se presiono Tecla D");
-            this.bird.x += 2;
+        } else if (this.TD.isDown) {
+            this.bird.setVelocityX(100);
             this.bird.flipX = false;
         }
+
         if (this.TW.isDown) {
-            //console.log("Se presiono Tecla W");
-            this.bird.y -= 2;
+            this.bird.setVelocityY(-100);
+        } else if (this.TS.isDown) {
+            this.bird.setVelocityY(100);
         }
-        if (this.TS.isDown) {
-            //console.log("Se presiono Tecla S");
-            this.bird.y += 2;
+
+        // Movimiento de bird2 (controlado con flechas)
+        if (this.cursors.left.isDown) {
+            this.bird2.setVelocityX(-100);
+            this.bird2.flipX = true;
+        } else if (this.cursors.right.isDown) {
+            this.bird2.setVelocityX(100);
+            this.bird2.flipX = false;
+        }
+
+        if (this.cursors.up.isDown) {
+            this.bird2.setVelocityY(-100);
+        } else if (this.cursors.down.isDown) {
+            this.bird2.setVelocityY(100);
         }
     }
-
 }
 
 export default Bootloader;
